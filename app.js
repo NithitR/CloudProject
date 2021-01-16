fb1238796d593e02df0e22d80c5ca492fbd317bf
nithit <nithit.r@vdc.co.th>
2021-01-16 13:49:07 +1100

    init

--- app.js
+++ app.js
@@ -5,15 +5,32 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('index', {user: 'FOO'});
+    res.render('index', {user: 'FOO'});
 })
 
 app.get('/analysis', function (req, res) {
-  res.render('main', {user: 'FOO'});
+    res.render('main', {user: 'FOO'});
 })
 
 app.get('/export', function (req, res) {
-  res.render('export', {user: 'FOO'});
+    res.render('export', {user: 'FOO'});
 })
+let port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
+app.listen(port, () => console.log("Example app listening on port " + port));
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'));
+function normalizePort(val) {
+    let port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
