ab66877514f59b9e98f54219e447c18657fd6dbb
nithit <nithit.r@vdc.co.th>
2021-01-16 13:32:19 +1100

    init

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-var app = express();
+app.get('/', function (req, res) {
+  res.render('index', {user: 'FOO'});
+})
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/analysis', function (req, res) {
+  res.render('main', {user: 'FOO'});
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/export', function (req, res) {
+  res.render('export', {user: 'FOO'});
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
