63c31f94d6a04ec35669c72962833c0c6e092f1e
nithit <nithit.r@vdc.co.th>
2021-01-16 13:45:25 +1100

    init

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+let express = require('express');
+let app = express();
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -30,25 +16,4 @@ app.get('/export', function (req, res) {
   res.render('export', {user: 'FOO'});
 })
 
-// app.listen(4000, () => console.log('Example app listening on port 4000!'));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
